81bb4d1160647e5a68f1c25d63276d2c9217dd4e
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2022-11-26 16:57:44 +0100

    Fix Command Loader

--- src/Zwuiix/AdvancedRank/commands/RankCommand.php
+++ src/Zwuiix/AdvancedRank/commands/RankCommand.php
@@ -4,6 +4,18 @@ namespace Zwuiix\AdvancedRank\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\Plugin;
+use Zwuiix\AdvancedRank\commands\sub\managePlayers\RankSubGive;
+use Zwuiix\AdvancedRank\commands\sub\managePlayers\RankSubSet;
+use Zwuiix\AdvancedRank\commands\sub\managePlayers\RankSubTempGive;
+use Zwuiix\AdvancedRank\commands\sub\managePlayers\RankSubUserRemovePermission;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\Manage;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\RankSubAddPermission;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\RankSubCreate;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\RankSubDelete;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\RankSubInfo;
+use Zwuiix\AdvancedRank\commands\sub\manageRanks\RankSubRemovePermission;
+use Zwuiix\AdvancedRank\commands\sub\others\RankSubList;
+use Zwuiix\AdvancedRank\commands\sub\RankSubUser;
 use Zwuiix\AdvancedRank\data\sub\PluginData;
 use Zwuiix\AdvancedRank\lib\CortexPE\Commando\BaseCommand;
 use Zwuiix\AdvancedRank\Main;
@@ -19,6 +31,7 @@ class RankCommand extends BaseCommand
     protected function prepare(): void
     {
         $optional = false;
+        /*
         $main=str_replace("Main", "", $this->plugin->getDescription()->getMain());
         $path=Main::getInstance()->getServer()->getDataPath()."plugins/{$this->plugin->getName()}/src/{$main}/commands/sub/";
         $scan = PathScanner::scanDirectory($path, ["php"]);
@@ -33,6 +46,21 @@ class RankCommand extends BaseCommand
             $this->registerSubCommand(new $name($optional));
             $i++;
         }
+        */
+
+        $this->registerSubCommand(new RankSubGive());
+        $this->registerSubCommand(new RankSubSet());
+        $this->registerSubCommand(new RankSubTempGive());
+        $this->registerSubCommand(new RankSubUserRemovePermission());
+        $this->registerSubCommand(new RankSubUserRemovePermission());
+        $this->registerSubCommand(new Manage());
+        $this->registerSubCommand(new RankSubAddPermission());
+        $this->registerSubCommand(new RankSubCreate());
+        $this->registerSubCommand(new RankSubDelete());
+        $this->registerSubCommand(new RankSubInfo());
+        $this->registerSubCommand(new RankSubRemovePermission());
+        $this->registerSubCommand(new RankSubList());
+        $this->registerSubCommand(new RankSubUser());
         $this->setPermission("advancedrank");
     }
 
